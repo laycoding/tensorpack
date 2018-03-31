e9e87f6b2f78e02d341f72315530bb60887c66f9
laycoding <battle975444818@outlook.com>
2018-03-31 16:37:08 +0800

    mass

--- examples/ResNet/HoMGR_utils.py
+++ examples/ResNet/HoMGR_utils.py
@@ -18,6 +18,7 @@ from tensorpack.utils.stats import RatioCounter
 from tensorpack.models import regularize_cost
 from tensorpack.tfutils.summary import add_moving_summary
 from tensorpack.utils import logger
+from tensorpack.dataflow.base import RNGDataFlow
 
 class HoMGRMeta(object):
 	"""
@@ -76,6 +77,7 @@ class HoMGRFiles(RNGDataFlow):
 	def __init__(self, dir, name, meta_dir=None, shuffle=None, dir_structure=None):
 		assert name in ['train', 'test', 'val'], name
 		assert os.path.isdir(dir), dir
+		self.full_dir = os.path.join(dir, name)
 
 		if shuffle is None:
 			shuffle = name == 'train'
@@ -117,6 +119,65 @@ class HoMGR(HoMGRFiles):
 			assert im is not None, fname
 			yield [im, label]
 
+class GoogleNetResize(imgaug.ImageAugmentor):
+    """
+    crop 8%~100% of the original image
+    See `Going Deeper with Convolutions` by Google.
+    """
+    def __init__(self, crop_area_fraction=0.08,
+                 aspect_ratio_low=0.75, aspect_ratio_high=1.333,
+                 target_shape=224):
+        self._init(locals())
+
+    def _augment(self, img, _):
+        h, w = img.shape[:2]
+        area = h * w
+        for _ in range(10):
+            targetArea = self.rng.uniform(self.crop_area_fraction, 1.0) * area
+            aspectR = self.rng.uniform(self.aspect_ratio_low, self.aspect_ratio_high)
+            ww = int(np.sqrt(targetArea * aspectR) + 0.5)
+            hh = int(np.sqrt(targetArea / aspectR) + 0.5)
+            if self.rng.uniform() < 0.5:
+                ww, hh = hh, ww
+            if hh <= h and ww <= w:
+                x1 = 0 if w == ww else self.rng.randint(0, w - ww)
+                y1 = 0 if h == hh else self.rng.randint(0, h - hh)
+                out = img[y1:y1 + hh, x1:x1 + ww]
+                out = cv2.resize(out, (self.target_shape, self.target_shape), interpolation=cv2.INTER_CUBIC)
+                return out
+        out = imgaug.ResizeShortestEdge(self.target_shape, interp=cv2.INTER_CUBIC).augment(img)
+        out = imgaug.CenterCrop(self.target_shape).augment(out)
+        return out
+
+def fbresnet_augmentor(isTrain):
+    """
+    Augmentor used in fb.resnet.torch, for BGR images in range [0,255].
+    """
+    if isTrain:
+        augmentors = [
+            GoogleNetResize(),
+            imgaug.RandomOrderAug(      # Remove these augs if your CPU is not fast enough
+                [imgaug.BrightnessScale((0.6, 1.4), clip=False),
+                 imgaug.Contrast((0.6, 1.4), clip=False),
+                 imgaug.Saturation(0.4, rgb=False),
+                 # rgb-bgr conversion for the constants copied from fb.resnet.torch
+                 imgaug.Lighting(0.1,
+                                 eigval=np.asarray(
+                                     [0.2175, 0.0188, 0.0045][::-1]) * 255.0,
+                                 eigvec=np.array(
+                                     [[-0.5675, 0.7192, 0.4009],
+                                      [-0.5808, -0.0045, -0.8140],
+                                      [-0.5836, -0.6948, 0.4203]],
+                                     dtype='float32')[::-1, ::-1]
+                                 )]),
+            imgaug.Flip(horiz=True),
+        ]
+    else:
+        augmentors = [
+            imgaug.ResizeShortestEdge(256, cv2.INTER_CUBIC),
+            imgaug.CenterCrop((224, 224)),
+        ]
+    return augmentors
 
 def get_HoMGR_dataflow(
         datadir, name, batch_size,
